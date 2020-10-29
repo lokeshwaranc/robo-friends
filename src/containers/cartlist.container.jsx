e8b7999b1a5fce21985ceb92b031473bd305382f
dev <lokeshwaran.c@gmail.com>
2020-10-29 23:41:51 +0530

    added key props to supress warnings

--- src/containers/cartlist.container.jsx
+++ src/containers/cartlist.container.jsx
@@ -5,7 +5,7 @@ import Card from '../components/card.component';
 const CardList = ({robots}) => {
 	return(
 		robots.map(robot=> (
-			<Card id={robot.id} name={robot.name} email={robot.email}/>
+			<Card key={robot.id} id={robot.id} name={robot.name} email={robot.email}/>
 		  ))
 	);
 }
